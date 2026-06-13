baa7943f21a369967145673d0a541a7f699e1d88
kingjangjo <araloveasalll@gmail.com>
2026-06-13 18:13:10 +0900

    Add Fishing & UIUpdating

--- Assets/01_Scripts/Fishing.cs
+++ Assets/01_Scripts/Fishing.cs
@@ -1,47 +1,55 @@
 using UnityEngine;
-using UnityEngine.UI;
 using System.Collections;
 
-public class Fishing : MonoBehaviour
+public class SimpleFishing : MonoBehaviour
 {
     [Header("UI 연결")]
-    [SerializeField] RectTransform bobber;
-    [SerializeField] RectTransform catchZone;
-    [SerializeField] float barWidth = 500f;
+    public RectTransform bobber;
+    public RectTransform catchZone;
+    public float barWidth = 500f;
 
-    [Header("설정")]
-    [SerializeField] float bobberSpeed = 200f;
-    [SerializeField] float zoneWidth = 120f;
+    public float rangeMin = 0.1f;
+    public float rangeMax = 0.9f;
+
+    public float period = 2.5f;
+
+    public float zoneMinWidth = 80f;
+    public float zoneMaxWidth = 140f;
 
     public event System.Action OnSuccess;
     public event System.Action OnFail;
 
-
     public void StartFishing() => StartCoroutine(FishingLoop());
-
 
     IEnumerator FishingLoop()
     {
-        float bobX = 0f;
-        float dir = 1f;
-        float zoneX = Random.Range(0f, barWidth - zoneWidth);
+        float rangeStartPx = rangeMin * barWidth;
+        float rangeEndPx = rangeMax * barWidth;
+        float rangePx = rangeEndPx - rangeStartPx;
 
-        catchZone.sizeDelta = new Vector2(zoneWidth, catchZone.sizeDelta.y);
+        float zoneW = Random.Range(zoneMinWidth, zoneMaxWidth);
+        float zoneX = rangeStartPx + Random.Range(0f, rangePx - zoneW);
+
+        catchZone.sizeDelta = new Vector2(zoneW, catchZone.sizeDelta.y);
         catchZone.anchoredPosition = new Vector2(zoneX, 0f);
+
+        float t = 0f;
 
         while (true)
         {
-            bobX += dir * bobberSpeed * Time.deltaTime;
-            if (bobX >= barWidth) { bobX = barWidth; dir = -1f; }
-            if (bobX <= 0f) { bobX = 0f; dir = 1f; }
+            t += Time.deltaTime / period;
+
+            float norm = (1f - Mathf.Cos(t * Mathf.PI * 2f)) * 0.5f;
+            float bobX = Mathf.Lerp(rangeStartPx, rangeEndPx, norm);
 
             bobber.anchoredPosition = new Vector2(bobX, 0f);
 
             if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
             {
-                bool inZone = bobX >= zoneX && bobX <= zoneX + zoneWidth;
-                if (inZone) OnSuccess?.Invoke();
-                else OnFail?.Invoke();
+                bool inZone = bobX >= zoneX && bobX <= zoneX + zoneW;
+                if (inZone) { OnSuccess?.Invoke(); Debug.Log("Success"); }
+                else { OnFail?.Invoke(); Debug.Log("Fail"); }
+                this.gameObject.SetActive(false);
                 yield break;
             }
 
--- Assets/01_Scripts/UIManager.cs
+++ Assets/01_Scripts/UIManager.cs
@@ -14,27 +14,48 @@ public class UIManager : MonoBehaviour
     public Slider storoageSlider;
     public Text Day;
     public Text Time;
+
+    [Header("시계 설정")]
+    public float realSecondsPerHour = 25f;
+    public float startHour = 1f;
     private void Update()
     {
-        
+        updateUI();
     }
     public void updateStorage()
     {
+        storoageSlider.value = (float)GameManager.instance.playerData.curStorage / GameManager.instance.playerData.maxStorage;
         storageText.text = GameManager.instance.playerData.curStorage.ToString() + " / " + GameManager.instance.playerData.maxStorage.ToString();
     }
     public void updateMoney()
     {
+        moneySlider.value = (float)GameManager.instance.playerData.curGold / GameManager.instance.playerData.maxGold;
         moneyText.text = GameManager.instance.playerData.curGold.ToString() + " / " + GameManager.instance.playerData.maxGold.ToString();
     }
     public void updateUI()
     {
         hpBar.fillAmount = (float)GameManager.instance.playerData.curHp / GameManager.instance.playerData.maxHp;
         fatigueBar.fillAmount = GameManager.instance.playerData.fatigue / 100f;
-        hpText.text = GameManager.instance.playerData.curHp.ToString() + " / " + GameManager.instance.playerData.maxHp.ToString();
-        fatigueText.text = GameManager.instance.playerData.fatigue.ToString("F1") + " / 100";
-        moneySlider.value = (float)GameManager.instance.playerData.curGold / GameManager.instance.playerData.maxGold;
-        storoageSlider.value = (float)GameManager.instance.playerData.curStorage / GameManager.instance.playerData.maxStorage;
-        Day.text = "Day " + GameManager.instance.playerData.day.ToString();
-        Time.text = (GameManager.instance.playerData.currentTime / 60f).ToString("F2") + "h";
+        hpText.text = GameManager.instance.playerData.curHp.ToString() + " / " + GameManager.instance.playerData.maxHp.ToString() + "\n체력";
+        fatigueText.text = GameManager.instance.playerData.fatigue.ToString("F1") + " / 100" + "\n피로도";
+        //Day.text = "Day " + GameManager.instance.playerData.day.ToString();
+        //Time.text = (GameManager.instance.playerData.currentTime / 60f).ToString("F2") + "h";
+        updateTime();
+    }
+    public void updateTime()
+    {
+        float currentTime = GameManager.instance.playerData.currentTime;
+
+        float gameMinutes = currentTime * (60f / realSecondsPerHour) + startHour * 60f;
+
+        int totalMinutes = (int)gameMinutes % (24 * 60);
+        int hour24 = totalMinutes / 60;
+        int minute = totalMinutes % 60;
+
+        int hour12 = hour24 % 12;
+        if (hour12 == 0) hour12 = 12;
+        string period = hour24 < 12 ? "AM" : "PM";
+
+        Time.text = string.Format("{0:D2}:{1:D2} {2}", hour12, minute, period);
     }
 }
