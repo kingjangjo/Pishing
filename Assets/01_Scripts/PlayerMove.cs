4e4fc2826f386d14e3a4e4a3c7b559170d217104
kingjangjo <araloveasalll@gmail.com>
2026-06-22 16:58:54 +0900

    123456789

---
+++ Assets/01_Scripts/PlayerMove.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
